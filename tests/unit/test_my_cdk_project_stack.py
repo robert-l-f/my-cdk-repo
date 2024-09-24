6dce6b97fb6e12e51dda0f14e9e62a9c0ab7a5d5
Larry Fowler <homepc@Larrys-MacBook-Pro.local>
2024-09-24 15:05:00 -0400

    Test a change

--- tests/unit/test_my_cdk_project_stack.py
+++ tests/unit/test_my_cdk_project_stack.py
@@ -9,7 +9,7 @@ def test_sqs_queue_created():
     app = core.App()
     stack = MyCdkProjectStack(app, "my-cdk-project")
     template = assertions.Template.from_stack(stack)
-
+#
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
 #     })
